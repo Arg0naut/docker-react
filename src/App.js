28c16202cf22e4230aa24d64fd1f37ea493f47bf
Arg0naut <Arg0nuat@test.com>
2018-09-05 02:47:43 +0100

    changing test app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">Hi There! Connnor Wonnor</p>
+        <p className="App-intro">Hi There!</p>
       </div>
     );
   }
